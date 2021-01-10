fa43fc8ebee348cd50ba6975b003998d596ede0a
ericgaspar <junk.eg@free.fr>
2021-01-10 10:47:59 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -18,3 +18,22 @@ pkg_dependencies=""
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+# Check the architecture
+#
+# example: architecture=$(ynh_detect_arch)
+#
+# usage: ynh_detect_arch
+#
+# Requires YunoHost version 2.2.4 or higher.
+ynh_detect_arch(){
+        local architecture
+        if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
+                architecture="arm64"               
+        elif [ -n "$(uname -m | grep 64)" ]; then
+                architecture="x86-64"
+        else
+                architecture="unknown"
+        fi
+        echo $architecture
+}
