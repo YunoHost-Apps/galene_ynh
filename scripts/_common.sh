289e896fa99719104ebd0076734e739dfaa98e0b
yalh76 <yalh@yahoo.com>
2022-10-05 00:36:46 +0200

    Implement LDAP authentication

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,11 @@
 # COMMON VARIABLES
 #=================================================
 
+go_version=1.19
+
+# dependencies used by the app (must be on a single line)
+pkg_dependencies="jose"
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
