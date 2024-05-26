dd969a02d239bf7914c0b27741cc69e962955498
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-05-26 21:44:05 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -17,11 +17,3 @@ go_version=1.20
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
-
-ynh_go_try_bash_extension() {
-  if [ -x src/configure ]; then
-    src/configure && make -C src || {
-      ynh_print_info --message="Optional bash extension failed to build, but things will still work normally."
-    }
-  fi
-}
